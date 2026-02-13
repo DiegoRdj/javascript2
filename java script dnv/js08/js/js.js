2b7ee608d38a63cc30351ac576412eed90b1a977
Diego R <diegorolimdejesus@gmail.com>
2026-02-13 15:50:18 -0300

    bump

---
+++ java script dnv/js08/js/js.js
@@ -0,0 +1,11 @@
+ //hosting - içamento
+ 
+ var nome = null
+ var sobrenome ='bastita'
+
+ console.log(nome)
+ console.log(sobrenome)
+
+ nome = 'matheus'
+ 
+ console.log(nome)
---
+++ java script dnv/js09/js/js.js
@@ -0,0 +1,12 @@
+var obj = {
+    nome: 'mat',
+    idade: 20,
+    profissao :'programador',
+
+}
+console.log(obj)
+console.log(obj.nome)
+console.log(obj.idade)
+console.log(obj.profissao)
+
+console.log('meu nome é' + obj.profissao)
